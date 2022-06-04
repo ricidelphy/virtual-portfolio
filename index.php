242e335725ea585ea35647e04a3372db3bc25bee
ricidelphy <rici.delphy@gmail.com>
2022-06-04 13:22:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="copyright" content="MACode ID, https://www.macodeid.com/">
+
+    <title>Portfolio</title>
+
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/themify-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vendor/owl-carousel/owl.carousel.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vendor/perfect-scrollbar/css/perfect-scrollbar.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vendor/nice-select/css/nice-select.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vendor/fancybox/css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/virtual.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/topbar.virtual.css">
+</head>
+
+<body class="theme-red">
+
+    <!-- Back to top button -->
+    <div class="btn-back_to_top">
+        <span class="ti-arrow-up"></span>
+    </div>
+
+    <div class="vg-page page-home" id="home" style="background-image: url(https://storage.googleapis.com/demo-bucket-rizki2022/assets/bg_image_1.jpg)">
+        <!-- Caption header -->
+        <div class="caption-header text-center wow zoomInDown">
+            <h5 class="fw-normal">Welcome</h5>
+            <h1 class="fw-light mb-4">I'm <b class="fg-theme">Rizki</b> Pratama</h1>
+            <div class="badge">Fullstack</div>
+        </div>
+        <!-- End Caption header -->
+        <div class="floating-button"><span class="ti-mouse"></span></div>
+    </div>
+
+    <div class="vg-page page-about" id="about">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-lg-4 py-3">
+                    <div class="img-place wow fadeInUp">
+                        <img src="https://storage.googleapis.com/demo-bucket-rizki2022/assets/rizki.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 offset-lg-1 wow fadeInRight">
+                    <h1 class="fw-light">Rizki Pratama</h1>
+                    <h5 class="fg-theme mb-3">Fullstack</h5>
+                    <p class="text-muted">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form but the majority have suffered alteration in some</p>
+                    <ul class="theme-list">
+                        <li><b>From:</b> Lhokseumawe, Aceh</li>
+                        <li><b>Lives In:</b> Aceh, Indonesia</li>
+                        <li><b>Age:</b> 32</li>
+                        <li><b>Gender:</b> Male</li>
+                    </ul>
+                    <button class="btn btn-theme-outline">Download CV</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Footer -->
+    <div class="vg-footer">
+        <h1 class="text-center">Social Media</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 py-3">
+                    <div class="footer-info">
+                        <p>Where to find me</p>
+                        <hr class="divider">
+                        <p class="fs-large fg-white">Jl. Satelit, Hagu Teungoh, Banda Sakti, Lhokseumawe - ACEH</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 py-3">
+                    <div class="float-lg-right">
+                        <p>Follow me</p>
+                        <hr class="divider">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Youtube</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 py-3">
+                    <div class="float-lg-right">
+                        <p>Contact me</p>
+                        <hr class="divider">
+                        <ul class="list-unstyled">
+                            <li>rici.delphy@gmail.com</li>
+                            <li>+62 852 7766 3642</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- End footer -->
+
+
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/vendor/owl-carousel/owl.carousel.min.js"></script>
+
+    <script src="assets/vendor/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+
+    <script src="assets/vendor/isotope/isotope.pkgd.min.js"></script>
+
+    <script src="assets/vendor/nice-select/js/jquery.nice-select.min.js"></script>
+
+    <script src="assets/vendor/fancybox/js/jquery.fancybox.min.js"></script>
+
+    <script src="assets/vendor/wow/wow.min.js"></script>
+
+    <script src="assets/vendor/animateNumber/jquery.animateNumber.min.js"></script>
+
+    <script src="assets/js/topbar-virtual.js"></script>
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIA_zqjFMsJM_sxP9-6Pde5vVCTyJmUHM&callback=initMap"></script>
+
+</body>
+
+</html>
